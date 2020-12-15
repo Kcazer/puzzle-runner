b8e1bceea5c708e6b5abf8f688c7a9a064555606
Kcazer <kcazer@gmail.com>
2020-12-15 13:14:53 +0000

    AOC2020 - Day 15

---
+++ advent-of-code/2020/day15/part1.ts
@@ -0,0 +1,16 @@
+export default ({ input }: Input) => {
+  const size = 2020;
+  const data = input.split(",").map(Number);
+
+  const indexes = new Array<number>(size).fill(0);
+  data.slice(0, -1).forEach((v, i) => indexes[v] = i + 1);
+
+  let value = data[data.length - 1];
+  for (let i = data.length; i < size; i++) {
+    const latest = indexes[value];
+    indexes[value] = i;
+    value = latest ? i - latest : 0;
+  }
+
+  return value;
+};
