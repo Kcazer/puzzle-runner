ed2ad1faf17be09ce0ea8798f3e4f9f9c24645fb
Kcazer <kcazer@gmail.com>
2022-12-02 08:38:00 +0000

    AOC2022 - Day 02

---
+++ advent-of-code/2022/day02/part1.ts
@@ -0,0 +1,22 @@
+export default ({ input }: Input) => {
+  // Typings
+  type Elf = "A" | "B" | "C";
+  type You = "X" | "Y" | "Z";
+  type Round = [Elf, You];
+
+  // Parse input
+  const data = input.split("\n").map((v) => v.split(" ")) as Round[];
+
+  // Scoring
+  const shape = { R: 1, P: 2, S: 3 };
+  const round = { RR: 3, RP: 0, RS: 6, PR: 6, PP: 3, PS: 0, SR: 0, SP: 6, SS: 3 };
+
+  // Mappers
+  const elfMove = ({ A: "R", B: "P", C: "S" } as const);
+  const youMove = ({ X: "R", Y: "P", Z: "S" } as const);
+
+  return data
+    .map(([e, y]) => [elfMove[e], youMove[y]])
+    .map(([e, y]) => shape[y] + round[`${y}${e}`])
+    .reduce((a, b) => a + b, 0); // Sum
+};
